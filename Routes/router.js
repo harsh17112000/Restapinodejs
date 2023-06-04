6b6bdda270006f4ecbea8deb67e000a77c085759
harsh17112000 <77334392+harsh17112000@users.noreply.github.com>
2023-06-04 11:28:30 +0530

    change

--- Routes/router.js
+++ Routes/router.js
@@ -7,7 +7,7 @@ router.post("/user/register",controllers.userpost);
 router.get("/user/getAlluser",controllers.getUsers);
 router.get("/user/singleuser/:id",controllers.getSingleuser);
 router.delete("/user/deleteuser/:id",controllers.deleteuser);
-router.put("/user/updateuser/:id",controllers.updateUser);
+router.put("/user/updateuser/:id",controllers.updateUser)
 
 
 module.exports = router;
